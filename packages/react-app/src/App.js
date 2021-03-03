92f782add5a889a3a325fcade1c7aef677fa302e
pancakehero-coder <64757105+pancakehero-coder@users.noreply.github.com>
2021-03-03 12:44:36 +0100

    first commit

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,73 +1,114 @@
 import React from "react";
-import { Contract } from "@ethersproject/contracts";
-import { getDefaultProvider } from "@ethersproject/providers";
-import { useQuery } from "@apollo/react-hooks";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { createMuiTheme, responsiveFontSizes, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid"
+import MaterialSwitch from '@material-ui/core/Switch';
+import { purple, green, blue, orange } from '@material-ui/core/colors';
 
-import { Body, Button, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+
+import { useWeb3React } from "@web3-react/core";
 import useWeb3Modal from "./hooks/useWeb3Modal";
-
 import { addresses, abis } from "@project/contracts";
-import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
 
-function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
-  return (
-    <Button
-      onClick={() => {
-        if (!provider) {
-          loadWeb3Modal();
-        } else {
-          logoutOfWeb3Modal();
-        }
-      }}
-    >
-      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
-    </Button>
-  );
-}
+import Header from './components/header/Header'
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TRANSFERS);
-  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+  const [ provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+  const [ darkMode, setDarkmode ] = React.useState(false)
+  const { account, library } = useWeb3React();
+  
+  var theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        // Purple and green play nicely together.
+        main: "#63474D",
+      },
+      secondary: {
+        // This is green.A700 as hex.
+        main: '#FFA686',
+      },
+      background: {
+        default: '#FEC196' ,
+        dark: '#63474D'
+      }
+    },
+  });
+  
+  theme = responsiveFontSizes(theme);
 
-  React.useEffect(() => {
-    if (!loading && !error && data && data.transfers) {
-      console.log({ transfers: data.transfers });
+  const useStyles = makeStyles((theme) => ({
+    
+    body: {
+      display:'flex',
+      flexDirection:'column',
+    
+      backgroundColor: darkMode ? '#141414' : theme.palette.background.default,
+    },
+    container: {
+      display:'flex',
+      flexDirection:'column',
+      // width: "100vw",
+      minHeight: '90vh',
+    },
+    banner: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      
+    },
+    header: {
+     
+      minHeight: '8vh'
     }
-  }, [loading, error, data]);
+  }));
+
+  const classes = useStyles();
 
   return (
-    <div>
-      <Header>
-        <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
-      </Header>
-      <Body>
-        <Image src={logo} alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button hidden onClick={() => readOnChainData()}>
-          Read On-Chain Balance
-        </Button>
-        <Link href="https://ethereum.org/developers/#getting-started" style={{ marginTop: "8px" }}>
-          Learn Ethereum
-        </Link>
-        <Link href="https://reactjs.org">Learn React</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
-      </Body>
-    </div>
+    <ThemeProvider theme={theme}> 
+      <Router>
+        <Grid container className={classes.body}>
+          <Grid item className={classes.header}>
+            <Header 
+              className={classes.header}
+              title='Template'
+              nav1='nav'
+              nav2='nav'
+              nav3='nav'
+              nav4='nav'
+              provider={provider} 
+              loadWeb3Modal={loadWeb3Modal} 
+              logoutOfWeb3Modal={logoutOfWeb3Modal}
+              darkMode={darkMode}
+
+            />
+          <Grid item className={classes.banner}>
+            <Typography variant='caption' color='textPrimary'>Addres: </Typography>
+            <Typography variant='caption' color='textPrimary'>{account}</Typography>
+            <div>
+              {/* {darkMode ? 'DAY' : 'NIGHT'} */}
+              <MaterialSwitch
+                  checked={darkMode} onChange={()=>setDarkmode(!darkMode)}
+              />
+            </div>
+          </Grid>
+          <Grid container className={classes.container}>
+              <Switch>
+                <Route path="/" exact component={''}/>
+                <Route path="/presale"  component={''}/>
+                <Route path="/research"  component={''}/>
+                <Route path="/strains"  component={''}/>
+                <Route path="/generator"  component={''}/>
+              </Switch>
+          </Grid>
+        </Grid> 
+      </Grid> 
+    </Router>
+  </ThemeProvider>
   );
 }
 
---
+++ packages/react-app/src/components/header/Header.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { makeStyles, createMuiTheme } from "@material-ui/core/styles"
+
+import AppBar from "@material-ui/core/AppBar"
+import ToolBar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import WalletButton from "./WalletButton/WalletButton"
+
+import { Link } from 'react-router-dom'
+import Styled from 'styled-components'
+import Button from '../buttons/Button'
+
+
+
+const Header = ({title, nav1, nav2, nav3, nav4, provider, loadWeb3Modal, logoutOfWeb3Modal, darkMode}) => {
+    var theme = createMuiTheme({
+        palette: {
+          type: darkMode ? 'dark' : 'light',
+        },
+      });
+    
+    const useStyles = makeStyles((theme) => ({
+        header: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            backgroundColor: darkMode ? '#141414' : theme.palette.background.default,
+            borderBottom: `5px solid ${theme.palette.secondary.main}`
+        },
+        title: {
+            display: 'none',
+            color: theme.palette.text.primary,
+            fontVariantCaps: 'all-small-caps',
+            [theme.breakpoints.up('sm')]: {
+              display: 'block',
+        },
+        }
+      }));
+    
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar position='relative' color='transparent' elevation={0} >
+                <ToolBar className={classes.header}>
+                    <Wrapper>
+                        <Link to='/' style={classes.link}> 
+                            <Typography className={classes.title} variant="h3" noWrap>
+                                {title}
+                            </Typography>
+                        </Link>
+                    </Wrapper>
+
+                    <NavWrapper>
+                        <Link to={`/${nav1}`} style={classes.link}> 
+                            <Typography className={classes.title} variant="h5" noWrap>
+                                {nav1}
+                            </Typography>
+                        </Link>
+                        <Link to={`/${nav2}`} style={classes.link}> 
+                            <Typography className={classes.title} variant="h5" noWrap>
+                                {nav2}
+                            </Typography>
+                        </Link>
+                        <Link to={`/${nav3}`} style={classes.link}> 
+                            <Typography className={classes.title} variant="h5" noWrap>
+                                {nav3}
+                            </Typography>
+                        </Link>
+                        <Link to={`/${nav4}`} style={classes.link}> 
+                            <Typography className={classes.title} variant="h5" noWrap>
+                                {nav4}
+                            </Typography>
+                        </Link>
+                    </NavWrapper>
+                    <Wrapper>
+                        <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal}/>
+                    </Wrapper>
+                </ToolBar>
+            </AppBar>  
+        </div>
+    )
+};
+
+export default Header;
+
+const Wrapper = Styled.div`
+    display: flex;
+    justify-content: center;
+    min-width: 20%;
+`
+
+const NavWrapper = Styled.div`
+    display: flex;
+    justify-content: inherit;
+    min-width: 40%;
+`
